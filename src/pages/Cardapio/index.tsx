64c0a5b2ee935237bea7863f17f221dda18df8b1
william andrade de souza <williamstrayder@gmail.com>
2022-07-28 15:00:41 -0300

    Adcionado svg como componente

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Cardapio.module.scss';
+import {ReactComponent as  Logo} from 'assets/logo.svg';
+
+function Cardapio() {
+  return (
+    <main>
+        <nav className={styles.menu}>
+        <Logo />
+        </nav>
+    </main>
+  )
+}
+
+export default Cardapio
